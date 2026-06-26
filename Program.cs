08dbdba1ba1ad642a29b2802ed99a70bd3fa5cb1
Alexandro-dev <mercadogarciaalexandro10@gmail.com>
2026-06-25 21:03:58 -0500

    automatizar despliegue a soome

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 
 // INTEGRACIÓN SENIOR: Activamos el soporte para clientes HTTP (Necesario para consultar SUNAT)
 builder.Services.AddHttpClient();
