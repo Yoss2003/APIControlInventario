cdb273c428f03069afb7135ad04b484dde15e384
Alexandro-dev <mercadogarciaalexandro10@gmail.com>
2026-06-25 20:02:15 -0500

    Add permisos

---
+++ Controllers/PermissionsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using InventoryAPI.Data;
+using ControlInventario.Shared.Models;
+
+namespace InventoryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PermissionsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PermissionsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Permissions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Permission>>> GetPermissions()
+        {
+            return await _context.Permissions.ToListAsync();
+        }
+    }
+}
--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -29,6 +29,8 @@ namespace InventoryAPI.Data
         // 3. Usuarios y Personal
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<RolePermission> RolePermissions { get; set; }
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Profile> Profiles { get; set; }
